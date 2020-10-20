8d26b8063969c92a602dc84304f9eaf928fd8748
Miaoqi0 <oumiaoqi@163.com>
2020-10-21 00:03:39 +0800

    Site updated: 2020-10-21 00:03:39

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,8 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?d5fc184a0596ccc00aa745fcccc5ca94";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
